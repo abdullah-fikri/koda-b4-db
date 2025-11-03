9a427e09ed107f796f5539871b9000733cc5e778
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-03 13:35:03 +0700

    feat: added product, categories and product_img table

---
+++ db/000003_create_product_and_categories_table.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE product_img;
+
+DROP TABLE products;
+
+DROP TABLE categories;
---
+++ db/000003_create_product_and_categories_table.up.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name varchar(100), 
+    description TEXT,
+    price NUMERIC(10,2),
+    stock INT,
+    category_id BIGINT,
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+
+CREATE TABLE product_img (
+    id SERIAL PRIMARY KEY,
+    image VARCHAR(150),
+    product_id BIGINT,
+    FOREIGN KEY(product_id) REFERENCES products(id),
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
