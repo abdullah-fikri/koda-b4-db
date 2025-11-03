21ef9404aab8475f38bfc2bd6d897ab51722c91d
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-03 12:02:33 +0700

    https://github.com/abdullah-fikri/koda-b4-db.git

---
+++ db/000001_create_users_table.up.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    email varchar(100) UNIQUE NOT NULL,
+    password TEXT,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE profile (
+    id SERIAL PRIMARY KEY,
+    users_id BIGINT,
+    username varchar(100),
+    tes varchar(100),
+    phone VARCHAR(20),
+    address VARCHAR(100),
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
